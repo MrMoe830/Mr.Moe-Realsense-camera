8fc6c3941b58fb2e9411fcb053ee4624b641727d
Mr.Moe <79902739+w-1016-Papman@users.noreply.github.com>
2022-03-13 00:15:53 +0800

    Update V_distance1.0_dm.py

--- realsense-example-python/V_distance1.0_dm.py
+++ realsense-example-python/V_distance1.0_dm.py
@@ -1,3 +1,4 @@
+"""双通道"""
 import pyrealsense2 as rs
 import numpy as np
 from cv2 import cv2
@@ -16,15 +17,15 @@ if __name__ == '__main__':
 
     if DEVICE_ID:
         config.enable_device(DEVICE_ID)
-
-    # 分辨率设置
+    
+    # 获取设备信息以便调整分辨率设置    
     pipe_wrapper = rs.pipeline_wrapper(pipe)
     pipe_profile = config.resolve(pipe_wrapper)
     device = pipe_profile.get_device()
     device_product_line = device.get_info(rs.camera_info.product_line)
     # print(device_product_line)
 
-    # enable stream
+    # 配置并启用指定流
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
     if device_product_line == 'L500':
         config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
@@ -44,33 +45,36 @@ if __name__ == '__main__':
         imu_profile = imu_pipe.start(imu_config)
         print(imu_profile.get_stream(rs.stream.gyro, ).as_motion_stream_profile().get_motion_intrinsics().data)
 
-    # 前十帧缓冲
+    # 前十帧缓冲，主要是留给相机的曝光、适应环境的时间
     for i in range(10):
         pipe.wait_for_frames(500 if i >= 1 else 10000)
 
-    align_to = rs.align(rs.stream.color)  # 对齐样式
+    align_to = rs.align(rs.stream.color)  # 对齐到RGB图像
     while True:
-        frames = pipe.wait_for_frames()
-        # print(frames.size())
+        frames = pipe.wait_for_frames()     # 等待一组可用帧到达
         align_frames = align_to.process(frames)
-        depth_frames = align_frames.get_depth_frame()
-        color_image = np.asanyarray(align_frames.get_color_frame().get_data())
-        depth_image = np.asanyarray(depth_frames.get_data())
-        colorizer_depth = np.asanyarray(colorizer.colorize(depth_frames).get_data())
-        # distance = depth_frames.get_distance(depth_frames.width//2,depth_frames.height//2)
+        depth_frames = align_frames.get_depth_frame() 
+        
+        # 将帧框架转换为numpy数组的可视化形式
+        color_image = np.asanyarray(align_frames.get_color_frame().get_data())  
+        depth_image = np.asanyarray(depth_frames.get_data())    # 未着色深度帧
+        colorizer_depth = np.asanyarray(colorizer.colorize(depth_frames).get_data())    # 着色后的深度帧
+     
+        # 获取指定点的距离
         distance = depth_image[int(depth_image.shape[0] / 2), int(depth_image.shape[1] / 2)]
         print(distance)
         center = [int(depth_image.shape[1] / 2), int(depth_image.shape[0] / 2)]
         cv2.circle(color_image, center, 3, [0, 0, 255], -1, 2)
         cv2.putText(color_image, f'{distance}mm', [center[0] + 10, center[1] - 5], cv2.FONT_HERSHEY_COMPLEX, 1,
                     [255, 255, 0], 1, 2, False)
-        # cv2.imshow('colorizer_depth', colorizer_depth)
         cv2.imshow('RealSense D455', color_image)
         cv2.imshow('colorizer_depth',colorizer_depth)
         if ENABLE_IMU:
-            imu_frames = imu_pipe.wait_for_frames()
-            ts = imu_frames.timestamp
-            num_frame = imu_frames.get_frame_number()
+            imu_frames = imu_pipe.wait_for_frames()  # 等待一组可用运动帧到达
+            ts = imu_frames.timestamp       # 帧元时间戳
+            num_frame = imu_frames.get_frame_number()   # 帧号
+            
+            # 获取指定运动帧            
             acc_frame = imu_frames.first_or_default(rs.stream.accel, rs.format.motion_xyz32f)
             gyro_frame = imu_frames.first_or_default(rs.stream.gyro, rs.format.motion_xyz32f)
             print('IMU 参数:\n\tframe [{}] in [{}]:\n\taccel = {},\n\tgyro = {}'.format(
